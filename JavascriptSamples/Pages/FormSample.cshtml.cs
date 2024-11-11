f352516500447a99e3b6efb392a0d678a079f338
augustinekudiyirippil <augustinekudiyirippil@gmail.com>
2024-11-11 14:53:25 +0000

    Added some other functions too

---
+++ JavascriptSamples/Pages/FormSample.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JavascriptSamples.Pages
+{
+    public class FormSampleModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
